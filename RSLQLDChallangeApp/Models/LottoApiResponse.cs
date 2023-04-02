a80588826a73f3ad0361412b201f9429aa8de73f
Ostap Kornev <ostap.kornev@ashm.org.au>
2023-04-03 06:35:15 +1000

    add files

---
+++ RSLQLDChallangeApp/Models/LottoApiResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//defines the data model for the Lotto API response
+
+namespace RSLQLDChallangeApp.Models
+{
+ 
+    public class LottoApiResponse
+    {
+        public List<Draw> Draws { get; set; } = new List<Draw>();
+        public object ErrorInfo { get; set; }
+        public bool Success { get; set; }
+    }
+
+    // Product class represents a lottery product and contains information such as the product ID, display name, logo URL, and latest draw.
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string DisplayName { get; set; }
+        public string LogoUrl { get; set; }
+        public Draw LatestDraw { get; set; }
+    }
+
+    //The Draw class represents a lottery draw and contains information about it
+    public class Draw
+    {
+        public string ProductId { get; set; }
+        public int DrawNumber { get; set; }
+        public string DrawDisplayName { get; set; }
+        public DateTime DrawDate { get; set; }
+        public string DrawLogoUrl { get; set; }
+        public string DrawType { get; set; }
+        public decimal Div1Amount { get; set; }
+        public bool IsDiv1Estimated { get; set; }
+        public bool IsDiv1Unknown { get; set; }
+        public DateTime DrawCloseDateTimeUTC { get; set; }
+        public DateTime DrawEndSellDateTimeUTC { get; set; }
+        public double DrawCountDownTimerSeconds { get; set; }
+    }
+}
---
+++ RSLQLDChallangeApp/ViewModels/LotteryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RSLQLDChallangeApp.Models;
+
+//This view model is intended to be used to pass data from the controller to the view 
+
+namespace RSLQLDChallangeApp.ViewModels
+{
+    public class LotteryViewModel
+    {
+        public List<Draw> Draws { get; set; }
+    }
+}
